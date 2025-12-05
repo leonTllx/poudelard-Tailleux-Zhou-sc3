aee2b65ab3a4397409ddfee7b5e9cc9bdf560a40
jkikizhou <killian.zhou.1@efrei.net>
2025-12-05 11:58:44 +0100

    creer fonction pour le texte

--- poudelard/main.py
+++ poudelard/main.py
@@ -1,4 +1,7 @@
-from utils import input_utils
+if __name__ == "__main__":
 
 reponse= input.utils.demander_texte("Entrez un texte")
 print("Vous avez saisie", reponse)
+
+nombre = input.utils.demander_nombre("Entrez une valeur")
+print("Vous avez saisi :", nombre)
--- poudelard/utils/input_utils.py
+++ poudelard/utils/input_utils.py
@@ -1,12 +1,3 @@
-nom = input("Entrez le nom de votre personnage : ")
-choix = int(input("Niveau de courage (1-10) : "))
-if choix < 1 or choix > 10:
-    while choix < 1 or choix > 10:
-        int(input("Veuillez entrer un nombre entre 1 et 10. "))
-print("Voulez-vous continuer ?")
-print("1. Oui")
-print("2. Non")
-choix_continuer = input("Votre choix")
 
 def demander_texte(message):
     texte = input(message).strip()
@@ -14,3 +5,13 @@ def demander_texte(message):
         print("Erreur, la saisie ne peut pas être vide")
         texte = input(message).strip()
     return texte
+
+def demander_nombre():
+    min_val = 1
+    max_val = 10
+    val = int(input("Veuillez entrer un nombre entre 1 et 10"))
+    while val < min_val or val > max_val:
+        print(f"Erreur, veuillez saisir une valeur entre {min_val} et {max_val}.")
+        val = int(input("Veuillez entrer un nombre"))
+    return val
+
