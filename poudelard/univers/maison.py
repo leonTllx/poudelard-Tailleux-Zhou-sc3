af38a84557a9039d24ae2981e47c91e31b0ec224
LeonTllx <leon.tailleux@efrei.net>
2025-12-05 11:26:17 +0100

    test

--- poudelard/univers/maison.py
+++ poudelard/univers/maison.py
@@ -1,1 +1,14 @@
 _NOM_MAISONS= ("Gryffondor","Serpentard","Poufsouffle","Serdaigle")
+def actualiser_points_maison(maison, nom_maison, points):
+    if nom_maison in maison:
+        maison[nom_maison] += points
+        print(f"{nom_maison} recoit {points} points. Total : {maison[nom_maison]}")
+    else:
+        print("Maison introuvable.")
+
+def afficher_maison_gagnante(maison):
+    if not maisons:
+        print("Aucune maison")
+        return
+    max_score = None
+    gagnantes = []
