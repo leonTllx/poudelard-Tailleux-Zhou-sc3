2121c1bf35d844f89a9234ac9be201c84ec92318
LeonTllx <leon.tailleux@efrei.net>
2025-12-05 10:07:59 +0100

    test

--- poudelard/univers/personnage.py
+++ poudelard/univers/personnage.py
@@ -1,1 +1,19 @@
-Stats_joueur = {"Nom": ,"Prenom":, "Argent": ,"Inventaire": ,"Sortilèges": ,"Attri"}
+def stats_joueur(nom, prenom):
+    nom = input(nom)
+    prenom = input(prenom)
+    print(prenom)
+    print(nom)
+"""
+Stats_joueur = {
+    "Nom":nom,
+    "Prenom":prenom,
+    "Argent":100,
+    "Inventaire":(),
+    "Sortilèges":(),
+    "Attributs":attributs
+}"""
+
+print("Stats_joueur")
+print("Stats_joueur")
+
+
