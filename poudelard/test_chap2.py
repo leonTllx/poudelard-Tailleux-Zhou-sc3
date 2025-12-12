9d693f4bf9cce1f258c54b48c3ae060e0bc3455d
jkikizhou <killian.zhou.1@efrei.net>
2025-12-12 12:13:44 +0100

    chapitre 1, j'ai fait

--- poudelard/test_chap2.py
+++ poudelard/test_chap2.py
@@ -1,4 +1,9 @@
+from poudelard.chapitres.chapitre_1 import introduction
+print("introduction")
+introduction()
+
+"""
 from poudelard.chapitres.chapitre_2 import rencontrer_amis
-rencontrer_amis()
+rencontrer_amis()"""
 
 
