38e0859810214ff49f62b4bcf0dfce4cbc75764e
jkikizhou <killian.zhou.1@efrei.net>
2025-12-19 08:35:05 +0100

    chapitre 1, j'ai fait

--- poudelard/test_chap2.py
+++ poudelard/test_chap2.py
@@ -1,8 +1,6 @@
 from poudelard.chapitres.chapitre_1 import *
-from poudelard.chapitres.chapitre_2 import*
-from poudelard.univers import personnage
 lancer_chapitre_1()
-lancer_chapitre_2(personnage)
+
 
 
 
