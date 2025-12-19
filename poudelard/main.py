d777f9317ab7ef7d2d2bf4b41cb29c58d6129183
LeonTllx <leon.tailleux@efrei.net>
2025-12-19 08:56:02 +0100

    patch note chap 3 + correction chap 2

--- poudelard/main.py
+++ poudelard/main.py
@@ -1,2 +1,12 @@
-from poudelard.chapitres.chapitre_1 import*
-lancer_chapitre_1()
+from poudelard.chapitres.chapitre_1 import lancer_chapitre_1
+from poudelard.chapitres.chapitre_2 import lancer_chapitre_2
+from poudelard.chapitres.chapitre_3 import lancer_chapitre_3
+
+def main():
+    personnage = lancer_chapitre_1()
+    maisons = {"Gryffondor": 0, "Serpentard": 0, "Poufsouffle": 0, "Serdaigle": 0}
+    personnage = lancer_chapitre_2(personnage)
+    lancer_chapitre_3(personnage, maisons)
+
+if __name__ == "__main__":
+    main()
